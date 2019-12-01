c006ed8d353568b2087d42f1f0551aba3b18dac1
JaiRaga <raga.j.santhosh@gmail.com>
2019-12-01 14:30:23 +0530

    refactored index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
 const express = require('express')
 require('./db/mongoose')
-const User = require('./models/user')
-const Task = require('./models/task')
 const userRouter = require('./routers/user')
 const taskRouter = require('./routers/task')
 
@@ -15,18 +13,4 @@ app.use(taskRouter)
 app.listen(port, () => {
     console.log(`Server is up on port ${port}`)
 })
-
-// const bcrypt = require('bcryptjs')
-// const myFunction = async () => {
-//     const password = 'red1234!'
-//     const hashedPass = await bcrypt.hash(password, 8)
-
-//     console.log(password)
-//     console.log(hashedPass)
-
-//     const isMatch = await bcrypt.compare('Red1234!', hashedPass)
-//     console.log(isMatch)
-// }
-
-// myFunction()
 
