88422bd14e1ae21aae85c4f9e9c7ba4c977462ff
JaiRaga <raga.j.santhosh@gmail.com>
2019-12-01 14:42:26 +0530

    Added tokens for auth

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -4,9 +4,9 @@ const User = require('../models/user')
 const auth = async (req, res, next) => {
     try {
         const token = req.header('Authorization').replace('Bearer ', '')
-        const decoded = jwt.verify(token, 'token')
-        const user = await User.findOne({ _id: decoded._id, "tokens.token": token })
-
+        const decode = jwt.verify(token, 'taskAppToken')
+        const user = await User.findOne({ _id: decode._id, "tokens.token": token })
+        
         if (!user) {
             throw new Error()
         }
@@ -14,7 +14,6 @@ const auth = async (req, res, next) => {
         req.token = token
         req.user = user
         next()
-
     } catch (e) {
         res.status(401).send({ error: "Please authenticate." })
     }
