7f21029029edfd9d6894a10b3886a6ba81358b8b
JaiRaga <raga.j.santhosh@gmail.com>
2019-12-01 14:38:32 +0530

    refactored routes to login/logout, added authentication and hashing

--- src/routers/user.js
+++ src/routers/user.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const User = require('../models/user')
+const auth = require('../middleware/auth')
 const router = new express.Router()
 
 // Create endpoint
@@ -8,24 +9,50 @@ router.post("/users", async (req, res) => {
 
     try {
         await user.save()
-        res.status(201).send(user)
+        const token = await user.generateAuthToken()
+
+        res.status(201).send({ user, token })
     } catch (e) {
         res.status(400).send(e)
     }
-
-    // user.save()
-    //     .then(() => res.status(201).send(user))
-    //     .catch((e) => res.status(400).send(e))
 })
 
-// Read endpoint
-router.get("/users", async (req, res) => {
+router.post("/users/login", async (req, res) => {
     try {
-        const user = await User.find({})
-        res.send(user)
+        const user = await User.findByCredentials(req.body.email, req.body.password)
+        const token = await user.generateAuthToken()
+        res.send({ user, token })
+    } catch (e) {
+        res.status(400).send()
+    }
+})
+
+router.post("/users/logout", auth, async (req, res) => {
+    try {
+        req.user.tokens = req.user.tokens.filter((token) => {
+            return token.token !== req.token
+        })
+
+        await req.user.save()
+        res.send()
     } catch (e) {
         res.status(500).send()
     }
+})
+
+router.post("/users/logoutAll", auth, async (req, res) => {
+    try {
+        req.user.tokens = []
+        await req.user.save()
+        res.send()
+    } catch (e) {
+        res.status(500).send()
+    }
+})
+
+// Read endpoint
+router.get("/users/me", auth, async (req, res) => {
+    res.send(req.user)
 })
 
 router.get("/users/:id", async (req, res) => {
@@ -43,8 +70,8 @@ router.get("/users/:id", async (req, res) => {
 })
 
 // Update endpoint
-router.patch("/users/:id", async (req, res) => {
-    const _id = req.params.id
+router.patch("/users/me", auth, async (req, res) => {
+    const _id = req.user._id
     const updates = Object.keys(req.body)
     const allowedUpdates = ["name", "age", "email", "password"]
     const isValidOperation = updates.every((update) => allowedUpdates.includes(update))
@@ -52,37 +79,21 @@ router.patch("/users/:id", async (req, res) => {
     if (!isValidOperation) {
         res.status(400).send({ error: 'Invalid updates' })
     }
-
-    // console.log(req.params, req.body, req)
     
     try {
-        const user = await User.findById(_id)
-
-        updates.forEach((update) => user[update] = req.body[update])
-        await user.save()
-
-        // const user = await User.findByIdAndUpdate(_id, req.body, { new: true, runValidators: true })
-
-        if (!user) {
-            res.status(404).send()
-        }
-
-        res.send(user)
+        updates.forEach((update) => req.user[update] = req.body[update])
+        await req.user.save()
+        res.send(req.user)
     } catch (e) {
         res.status(400).send()
     }
 })
 
 // Delete endpoint
-router.delete("/users/:id", async (req, res) => {
+router.delete("/users/me", auth, async (req, res) => {
     try {
-        const user = await User.findByIdAndDelete(req.params.id)
-
-        if (!user) {
-            res.status(404).send()
-        }
-
-        res.send(user)
+        await req.user.remove()
+        res.send(req.user)
     } catch (e) {
         res.status(500).send()
     }
