3eceedfd728a079ccf45f8084ce3c74b9130f92e
JaiRaga <raga.j.santhosh@gmail.com>
2020-05-04 20:08:14 +0530

    created routes for signing-in new and existing user

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Login from "./component/auth/Login";
+import Register from "./component/auth/Register";
+import setAuthToken from "./utils/setAuthToken";
+import { loadUser } from "./actions/auth";
+
+import "./App.css";
+
+// Redux
+import { Provider } from "react-redux";
+import store from "./store";
+import Dashboard from "./component/layout/Dashboard";
+
+if (localStorage.token) {
+  setAuthToken(localStorage.token);
+}
 
 function App() {
+  useEffect(() => {
+    store.dispatch(loadUser());
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={store}>
+      <Router>
+        <div className='App'>
+          <Switch>
+            <Route exact path='/register' component={Register} />
+            <Route exact path='/login' component={Login} />
+            <Route exact path='/dashboard' component={Dashboard} />
+          </Switch>
+        </div>
+      </Router>
+    </Provider>
   );
 }
 
