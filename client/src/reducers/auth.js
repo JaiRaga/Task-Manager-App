20aad43ed8aa93e526267d66687083c4ee09f154
JaiRaga <raga.j.santhosh@gmail.com>
2020-05-04 20:03:35 +0530

    created reducer for registering and logging in user

--- client/src/reducers/auth.js
+++ client/src/reducers/auth.js
@@ -1,9 +1,12 @@
 import {
+  AUTH_ERROR,
   REGISTER_SUCCESS,
   REGISTER_FAILURE,
   LOGIN_SUCCESS,
   LOGIN_FAILURE,
-  LOGOUT
+  LOGOUT,
+  LOGOUT_ALL,
+  USER_LOADED
 } from "../actions/types";
 
 const initialState = {
@@ -13,4 +16,35 @@ const initialState = {
   loading: true
 };
 
-export default (state = initialState, action) => {};
+export default (state = initialState, action) => {
+  const { type, payload } = action;
+
+  switch (type) {
+    case USER_LOADED:
+      return {
+        ...state,
+        user: payload,
+        isAuthenticated: true,
+        loading: false
+      };
+    case REGISTER_SUCCESS:
+      localStorage.setItem("token", payload.token);
+      return {
+        ...state,
+        user: payload.token,
+        isAuthenticated: true,
+        loading: false
+      };
+    case AUTH_ERROR:
+      localStorage.removeItem("token");
+      return {
+        ...state,
+        user: null,
+        isAuthenticated: false,
+        loading: false
+      };
+
+    default:
+      return state;
+  }
+};
