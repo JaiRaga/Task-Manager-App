c8189e96038b8fb3826b6e12558d67ddc27d1076
JaiRaga <raga.j.santhosh@gmail.com>
2020-05-04 20:01:05 +0530

    added action types for login, register, auth error, loading user and logging out user

--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -1,5 +1,8 @@
+export const AUTH_ERROR = "AUTH_ERROR";
 export const LOGIN_SUCCESS = "LOGIN_SUCCESS";
 export const LOGIN_FAILURE = "LOGIN_FAILURE";
 export const REGISTER_SUCCESS = "REGISTER_SUCCESS";
 export const REGISTER_FAILURE = "REGISTER_FAILURE";
 export const LOGOUT = "LOGOUT";
+export const LOGOUT_ALL = "LOGOUT_ALL";
+export const USER_LOADED = "USER_LOADED";
