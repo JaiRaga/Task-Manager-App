b29d9c13781b545d08a8a5f5e586d65878209c3b
JaiRaga <raga.j.santhosh@gmail.com>
2020-05-04 20:06:54 +0530

    created component for signing-in existing user and connected to redux store

--- client/src/component/auth/Login.js
+++ client/src/component/auth/Login.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import PropTypes from "prop-types";
 
 const Login = (props) => {
@@ -17,7 +18,7 @@ const Login = (props) => {
         <a href='#'>Forgot Password</a>
       </div>
       <p>
-        Need an account? <a href='#'>Sign Up</a>
+        Need an account? <Link to='/register'>Sign Up</Link>
       </p>
     </div>
   );
