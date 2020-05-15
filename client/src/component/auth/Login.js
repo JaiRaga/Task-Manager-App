ff87ee3e9d5ed82baf2ef264bd0123aeee36fca9
JaiRaga <raga.j.santhosh@gmail.com>
2020-05-15 18:52:31 +0530

    connected with state and redux for updating login info

--- client/src/component/auth/Login.js
+++ client/src/component/auth/Login.js
@@ -1,22 +1,55 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { Link, Redirect } from "react-router-dom";
+import { login } from "../../actions/auth";
 import PropTypes from "prop-types";
 
 const Login = (props) => {
+  const isAuthenticated = useSelector((state) => state.auth.isAuthenticated);
+  const dispatch = useDispatch();
+  const [formData, setFormData] = useState({ email: "", password: "" });
+
+  const { email, password } = formData;
+
+  const onChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+
+    dispatch(login({ email, password }));
+  };
+
+  if (isAuthenticated) {
+    return <Redirect to='/dashboard' />;
+  }
+
   return (
-    <div class='login'>
-      <h1 class='heading'>Log In</h1>
-      <input class='input' type='text' name='email' placeholder='Your Email' />
-      <input
-        class='input'
-        type='password'
-        name='password'
-        placeholder='Your Registered Password'
-      />
-      <div class='btn reset'>
-        <input type='submit' value='Log In' />
-        <a href='#'>Forgot Password</a>
-      </div>
+    <div className='login'>
+      <h1 className='heading'>Log In</h1>
+      <form action='#' onSubmit={onSubmit}>
+        <input
+          className='input'
+          type='text'
+          name='email'
+          value={email}
+          onChange={onChange}
+          placeholder='Your Email'
+        />
+        <input
+          className='input'
+          type='password'
+          name='password'
+          value={password}
+          onChange={onChange}
+          placeholder='Your Registered Password'
+        />
+        <div className='btn reset'>
+          <input type='submit' value='Log In' />
+          <a href='#'>Forgot Password</a>
+        </div>
+      </form>
       <p>
         Need an account? <Link to='/register'>Sign Up</Link>
       </p>
@@ -24,6 +57,9 @@ const Login = (props) => {
   );
 };
 
-Login.propTypes = {};
+Login.propTypes = {
+  login: PropTypes.func.isRequired,
+  isAuthenticated: PropTypes.bool
+};
 
 export default Login;
